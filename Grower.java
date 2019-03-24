197d017afd271a92d220c466cd9919c6f7df9b27
Itai Neria <itai.neria@gmail.com>
2019-03-24 09:33:48 -0700

    Added Grower class

---
+++ Grower.java
@@ -0,0 +1,14 @@
+package flowerSimulation;
+
+public class Grower extends Person {
+	Gardener _gardener;
+	public Grower(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+	public FlowersBouquet requestFlowers(String flowers) {
+		System.out.println(""+this.getName()+" forwards request to "+_gardener.getName());
+		_gardener=_gardener.prepareBouquet(flowers);
+	}
+	
+}
