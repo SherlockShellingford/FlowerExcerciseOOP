226f076201f3a4308490b5e21843f4d8dbc59dd4
Itai Neria <itai.neria@gmail.com>
2019-03-24 10:24:00 -0700

    added WholeSaler class

---
+++ WholeSaler.java
@@ -0,0 +1,15 @@
+package flowerSimulation;
+
+public class WholeSaler extends Person {
+	Grower _grower;
+	public WholeSaler(String name, Grower grower) {
+		super(name);
+		_grower=grower;
+	}
+	public FlowerBouquet requestFlower(String flowers) {
+		System.out.println(""+this.getName()+" forwards request to "+_grower.getName());
+		_grower.requestFlowers(flowers);
+	}
+
+
+}
