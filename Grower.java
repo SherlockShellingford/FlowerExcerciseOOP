bfd763732ffa80d641b7c6a7944032e550a54fa1
Itai Neria <itai.neria@gmail.com>
2019-03-24 09:55:51 -0700

    added Grower class

--- Grower.java
+++ Grower.java
@@ -6,7 +6,7 @@ public class Grower extends Person {
 		super(name);
 		// TODO Auto-generated constructor stub
 	}
-	public FlowersBouquet requestFlowers(String flowers) {
+	public FlowerBouquet requestFlowers(String flowers) {
 		System.out.println(""+this.getName()+" forwards request to "+_gardener.getName());
 		_gardener=_gardener.prepareBouquet(flowers);
 	}
